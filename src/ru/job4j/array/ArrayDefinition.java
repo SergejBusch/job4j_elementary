1a011614915dfcc603d35353c0d23651070bda29
Mr. Anderson <h-blast@list.ru>
2020-06-15 23:53:06 +0200

    6.0.1. Объявление массива. [#305341]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,7 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    short[] shortArray = new short[10];
+    String[] stringArray = new String[100500];
+    float[] floatArray = new float[40];
+}
