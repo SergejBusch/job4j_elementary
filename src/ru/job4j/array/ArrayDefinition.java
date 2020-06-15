1fe70b6b9b7fa59f097fb470abeeb700236f603b
Mr. Anderson <h-blast@list.ru>
2020-06-15 23:58:31 +0200

    6.0.2. Заполнение массива. [#305332]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,4 +4,16 @@ public class ArrayDefinition {
     short[] shortArray = new short[10];
     String[] stringArray = new String[100500];
     float[] floatArray = new float[40];
+
+    public static void main(String[] args) {
+        String[] names = new String[4];
+        names[0] = "Johnny";
+        names[1] = "Bonny";
+        names[2] = "Tony";
+        names[3] = "Penny";
+
+        for (String string : names) {
+            System.out.println(string);
+        }
+    }
 }
