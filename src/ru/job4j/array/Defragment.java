c269a417b192942866310a8d9553ef56f4efcf40
Mr. Anderson <h-blast@list.ru>
2020-06-16 18:48:41 +0200

    6.8. Дефрагментация массива [#305340]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                for (int i = index; i < array.length; i++) {
+                    if (array[i] != null) {
+                        SwitchArray.swap(array, i, index);
+                        break;
+                    }
+                }
+                System.out.print(array[index] + " ");
+            }
+
+        }
+        return array;
+    }
+}
