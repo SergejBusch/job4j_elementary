6b8a5264ae708d412f234a02e763b5c91951797b
Mr. Anderson <h-blast@list.ru>
2020-06-18 11:35:01 +0200

    2.2. Refactoring - Triangle Square. [#305487]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,19 +1,21 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class TriangleTest {
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void areaTestWhenTriangleExist() {
+        double result = new Triangle(new Point(5, 6), new Point(1, 1), new Point(9, 9)).area();
+        double expected = 3.99;
+        Assert.assertEquals(expected, result, 0.01);
     }
 
     @Test
-    public void whenNotExist() {
-        boolean result = Triangle.exist(2.0, 0, 2.0);
-        assertThat(result, is(false));
+    public void areaTestWhenTriangleNotExist() {
+        double result = new Triangle(new Point(0, 0), new Point(0, 0), new Point(9, 9)).area();
+        assertThat(result, is(-1.0));
     }
 }
