eda300022b35bf5506e0f8f3df183a7ec3a6d488
Mr. Anderson <h-blast@list.ru>
2020-06-18 12:49:02 +0200

    2. Constructor overload [#305502]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -1,6 +1,21 @@
 package ru.job4j.tracker;
 
 public class Item {
+    private String id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    public Item(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public static void main(String[] args) {
         Item item = new Item();
     }
