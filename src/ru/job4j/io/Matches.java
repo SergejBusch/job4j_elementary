ec7cdff676a4276ca4d08363ec3c7144c7bae877
Mr. Anderson <h-blast@list.ru>
2020-06-21 15:26:43 +0200

    2. Scanner and reading the number from the console. [#305448]

--- src/ru/job4j/io/Matches.java
+++ src/ru/job4j/io/Matches.java
@@ -6,25 +6,18 @@ public class Matches {
     public static void main(String[] args) {
         int matches = 11;
         Scanner input = new Scanner(System.in);
-        int playerNumber = 0;
+        boolean playerNumber = true;
         while (true) {
-            System.out.println("Игрок " + (playerNumber++ % 2 + 1) + " ходит");
+            System.out.println("Игрок " + (playerNumber ? 1 : 2) + " ходит");
             System.out.println("возьмите от 1 до 3 спичек");
             int number = input.nextInt();
-            if (number == 1) {
-                matches -= 1;
+            if (number > 0 && number < 4) {
+                matches -= number;
                 System.out.println("Осталось " + matches + " спичек");
-            } else if (number == 2) {
-                matches -= 2;
-                System.out.println("Осталось " + matches + " спичек");
-            } else if (number == 3) {
-                matches -= 3;
-                System.out.println("Осталось " + matches + " спичек");
-            } else {
-                playerNumber++;
+                playerNumber = playerNumber ? false : true;
             }
             if (matches <= 0) {
-                System.out.println("Игрок " + (--playerNumber % 2 + 1) + " победил");
+                System.out.println("Игрок " + (playerNumber ? 2 : 1) + " победил");
                 break;
             }
         }
